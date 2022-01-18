136f78ed3d270ac6df83f3adb9a0da43ec655997
lielGlazer <lielglazer@gmail.com>
2022-01-18 17:34:29 +0200

    liel new

--- צד לקוח/‏‏תיקיה חדשה/app/src/app/app-routing.module.ts
+++ צד לקוח/‏‏תיקיה חדשה/app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JoinComponent } from './join/join.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"SignIn", component:SignInComponent},
+  {path:"Join",component:JoinComponent},
+  {path:"User",component:UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
