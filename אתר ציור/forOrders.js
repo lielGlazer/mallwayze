fd19c7edd86b132651d821cb1d86288ef8e2af48
liel <lielglazer@gmail.com>
2022-05-11 10:43:43 +0300

    רק השם עוזזזר 11.5

---
+++ אתר ציור/forOrders.js
@@ -0,0 +1,116 @@
+﻿const products = [
+   
+    {
+        id: 120,
+        name: 'מפוצץ',
+        image: "פרסומות/המון אנימה .png",
+        price: 150
+    },
+    {
+        id: 130,
+        name: 'מלחמה',
+        image: "פרסומות/אנימה מלחמה .png",
+        price: 200
+    },
+    {
+        id: 123,
+        name: 'צמד',
+        image: "פרסומות/אנימה צמד.png",
+        price: 100
+    },
+    
+    {
+        id: 110,
+        name: 'אנימה בכחול',
+        image: "פרסומות/אנימה כחולה .png",
+        price: 200
+    },
+    {
+        id: 120,
+        name: 'גוונים',
+        image: "פרסומות/מלחיצץ.png",
+        price: 150
+    },
+    {
+        id: 130,
+        name: 'קרב',
+        image: "פרסומות/צילום מסך 2021-08-02 181642.png",
+        price: 200
+    },
+    {
+        id: 123,
+        name: 'פרסומת',
+        image: "פרסומות/קווול.png",
+        price: 100
+    },
+     {
+        id: 123,
+        name: 'ילדים',
+         image: "פרסומות/קטנים.png",
+        price: 100
+    }
+];
+const cart = {
+    list: [],
+    sum: 0,
+    addCart(product) {
+        this.list.push(product);
+        this.sum += product.price;
+        const cart = document.getElementById('cart');
+        const li = document.createElement('li');
+        li.textContent = `${product.name} --> ${product.price}`;
+        cart.firstElementChild.appendChild(li);
+        cart.lastElementChild.textContent = this.sum + '$';
+    },
+    removeCart(productID) {
+        const productIndex = products.findIndex(p => p.id === productID);
+        this.list.splice(productIndex, 1); // מחיקת איבר אחד מהמקום האינדקס
+        this.sum -= product.price;
+        const cart = document.getElementById('cart');
+        cart.firstElementChild.children[productIndex].remove();
+        cart.lastElementChild.textContent = this.sum + '$';
+    }
+};
+
+const createHtmlProduct = (product) => {
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('card', 'mb-3', 'shadow', 'mb-5', 'bg-white', 'rounded', 'my');
+    mainDiv.style.width = '30rem'; 
+    mainDiv.style.height = '18rem';
+    mainDiv.setAttribute('data-id', product.id);
+    const img = document.createElement('img');
+    img.src = product.image;
+    img.classList.add('card-img-top');
+    img.alt = product.name;
+    const body = document.createElement('div');
+    body.classList.add('card-body');
+    const header = document.createElement('h3');
+    header.classList.add('card-title');
+    header.textContent = product.name;
+    const price = document.createElement('p');
+    price.classList.add('card-text');
+    price.append(document.createTextNode(product.price + '$'));
+    const btnOrder = document.createElement('a');
+    btnOrder.href = "#"; 
+    btnOrder.textContent = 'TAKE';
+    btnOrder.style.width = '50px';
+    btnOrder.style.height = '25px';
+    btnOrder.style.backgroundImage = "אנימה/גינס.png";
+    btnOrder.style.marginBottom = '20px';
+    const btnDetails = btnOrder.cloneNode(); 
+    btnDetails.textContent = 'עוד פרטים';
+    btnDetails.onclick = () => alert(product.name);
+    btnOrder.addEventListener('click', () => cart.addCart(product));
+    body.append(price, btnOrder,);
+    mainDiv.append(img, body);
+    return mainDiv;
+}
+
+const init = () => {
+    const storeDiv = document.getElementById('store');
+    const lstProducts = [];
+    for (let i = 0; i < products.length; i++) {
+        lstProducts.push(createHtmlProduct(products[i]));
+    }
+    storeDiv.append(...lstProducts);
+}
---
+++ אתר ציור/mami.js
@@ -0,0 +1,78 @@
+﻿
+const drow = [
+    {
+        id =10,
+        name ="עניים ",
+        image ="תמונות/droww/1 (2).jpg",
+        cost =120,
+    },
+    {
+        id =20,
+        name ="חצי  ",
+        image ="תמונות/droww/3.jpg",
+        cost =20,
+    },
+    {
+        id =30,
+        name ="אריה  ",
+        image ="תמונות/droww/אריה.jpg",
+        cost =200,
+    },
+    {
+        id =40,
+        name ="ברבי ",
+        image ="תמונות/droww/ברבי.jpg",
+        cost =120,
+    },
+    {
+        id =50,
+        name ="מעבר חצוי",
+        image ="תמונות/droww/חצי.jpg",
+        cost =300,
+    },
+    {
+        id =60,
+        name ="כיתובים  ",
+        image ="תמונות/droww/ליאל.jpg",
+        cost =600,
+    },
+    {
+        id =70,
+        name ="מאופרת  ",
+        image ="תמונות/droww/מאופרת.jpg",
+        cost =200,
+    },
+    {
+        id =80,
+        name ="מגניב  ",
+        image ="תמונות/droww/מטורף.jpg",
+        cost =500,
+    },
+    {
+        id =90,
+        name =" מתוקהה:)",
+        image ="תמונות/droww/מתוקה.jpg",
+        cost =260,
+    },
+    {
+        id =100,
+        name ="מתוקי ",
+        image ="תמונות/droww/מתוקי.jpg",
+        cost =300,
+    }
+]
+const cart = {
+    sum: 0,
+    list:[],
+}
+const addToCart = (drow) => {
+    cart.sum += drow.cost;
+    cart.list.push(drow);
+}
+
+
+
+
+
+    
+    
